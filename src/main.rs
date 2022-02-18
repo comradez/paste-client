3d86f4f183c34912f60ce64509e715d000a1672f
ChrisZhang <zcyjim@outlook.com>
2022-02-18 16:28:25 +0800

    改为 JSON 数据交换

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate clap;
 use clap::{Parser, Subcommand};
 use config::Config;
+use serde_derive::{Serialize, Deserialize};
 use std::io::{stdin, Read, Write};
 
 mod config;
@@ -13,9 +14,12 @@ fn get_url(path: &str) -> std::io::Result<Config> {
     Ok(config)
 }
 
-fn set_username(username: &str, path: &str, mut config: Config) -> std::io::Result<()> {
+fn set_username(username: &str, path: &str, base_url: &str) -> std::io::Result<()> {
     let mut f = std::fs::File::create(path)?;
-    config.username = Some(username.into());
+    let config = Config {
+        base_url: base_url.into(),
+        username: Some(username.into())
+    };
     f.write_all(toml::to_string(&config).unwrap().as_bytes())?;
     Ok(())
 }
@@ -49,29 +53,41 @@ enum Commands {
     Last
 }
 
+#[derive(Serialize, Deserialize)]
+struct ExchangeMessage {
+    content: String,
+    username: String,
+}
+
 fn main() -> std::io::Result<()> {
     let user_name = std::env::var("HOME").unwrap();
     let config_path = format!("{}/.config/paste-client/config.toml", user_name);
     let hist_path = format!("{}/.config/paste-client/history_token", user_name);
     let config = get_url(&config_path).unwrap();
-    let (base_url, _username) = (&config.base_url, config.username.as_ref().unwrap_or(&"Anonymous".into()));
+    let (base_url, username) = (config.base_url, config.username.unwrap_or("Anonymous".into()));
     let end_char = if cfg!(target_os = "windows") { 'Z' } else { 'D' };
 
     let cli = Cli::parse();
     match cli.command {
         Commands::Get { token } => {
             let url = format!("{}/{}", base_url, token);
-            println!("{}", reqwest::blocking::get(url).unwrap().text().unwrap());
+            let message: ExchangeMessage = serde_json::from_str(&reqwest::blocking::get(url).unwrap().text().unwrap()).unwrap();
+            println!("User {} pasted:", &message.username);
+            println!("{}", &message.content);
         },
         Commands::Send { message } => {
-            let message = message.unwrap_or_else(|| {
+            let content = message.unwrap_or_else(|| {
                 let mut content = String::new();
                 println!("Input the message. Ctrl + {} to end.", end_char);
                 stdin().read_to_string(&mut content).unwrap();
                 content
             });
+            let message = serde_json::to_string(&ExchangeMessage {
+                content,
+                username: username.into()
+            }).unwrap();
             let client = reqwest::blocking::Client::new();
-            let resp = client.post(base_url).body(message).send().unwrap();
+            let resp = client.post(&base_url).body(message).send().unwrap();
             let token = resp.text().unwrap();
             save_history(&token, &hist_path).expect("Failed to record history");
             println!("\n{}/{}", &base_url, &token);
@@ -82,9 +98,7 @@ fn main() -> std::io::Result<()> {
             let resp = client.delete(url).send().unwrap();
             println!("{}", resp.text().unwrap());
         },
-        Commands::Username { name } => {
-            set_username(&name, &config_path, config)?;
-        },
+        Commands::Username { name } => set_username(&name, &config_path, &base_url)?,
         Commands::Last => println!(
             "{}/{}",
             base_url,
